6e85d2f393c12b39bea7d22eca1ba604e5af5d12
amanischool2345-max <amanischool2345@gmail.com>
2026-01-25 21:32:25 +0300

    Add DATABASE_URL from environment variables

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+const DATABASE_URL = process.env.DATABASE_URL;
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
